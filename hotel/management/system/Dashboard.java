657554dadd1b683203add08763755f51284d92fb
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-08-20 16:25:08 +0200

    title in Dashboard

--- hotel/management/system/Dashboard.java
+++ hotel/management/system/Dashboard.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hotel.management.system;
 
-/**
- *
- * @author azizbelkhouja
- */
-public class Dashboard {
+import java.awt.*;
+import javax.swing.*;
+
+public class Dashboard extends JFrame {
     
+    Dashboard() {
+        setBounds(0,0,1550,1000);
+        
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/third.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1550, 1000, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 1550, 1000);
+        
+        JLabel text = new JLabel("Belkhouja Group WELCOMES YOU");
+        text.setBounds(400, 80, 1000, 50);
+        text.setFont(new Font("Tahoma", Font.PLAIN, 46));
+        text.setForeground(Color.WHITE);
+        image.add(text);
+        
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new Dashboard();
+    }
 }
