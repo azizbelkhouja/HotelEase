f50a336f95c83f98957316fde87fe317c514c759
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-08-20 16:52:24 +0200

    Dashboard updates

--- hotel/management/system/Dashboard.java
+++ hotel/management/system/Dashboard.java
@@ -15,12 +15,35 @@ public class Dashboard extends JFrame {
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
         image.setBounds(0, 0, 1550, 1000);
+        add(image);
         
-        JLabel text = new JLabel("Belkhouja Group WELCOMES YOU");
+        JLabel text = new JLabel("SIDI BOU HOTELS WELCOMES YOU");
         text.setBounds(400, 80, 1000, 50);
         text.setFont(new Font("Tahoma", Font.PLAIN, 46));
         text.setForeground(Color.WHITE);
         image.add(text);
+
+        JMenuBar mb = new JMenuBar();
+        mb.setBounds(0, 0, 1550, 30);
+        image.add(mb);
+        
+        JMenu hotel = new JMenu("HOTEL MANAGEMENT");
+        hotel.setForeground(Color.RED);
+        mb.add(hotel);
+        
+        JMenuItem reception = new JMenuItem("reception");
+        hotel.add(reception);
+        
+        JMenu admin = new JMenu("admin");
+        admin.setForeground(Color.BLUE);
+        mb.add(admin);
+        
+        JMenuItem addemployee = new JMenuItem("ADD EMPLOYEE");
+        admin.add(addemployee);
+        JMenuItem addrooms = new JMenuItem("ADD ROOMS");
+        admin.add(addrooms);
+        JMenuItem adddrivers = new JMenuItem("ADD DRIVERS");
+        admin.add(adddrivers);
         
         setVisible(true);
     }
