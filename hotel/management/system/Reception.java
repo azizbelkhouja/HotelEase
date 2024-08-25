16363723f0431bcbf4682c65cd33809adcee3d20
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-08-25 16:18:10 +0200

    Department infos

--- hotel/management/system/Reception.java
+++ hotel/management/system/Reception.java
@@ -40,60 +40,70 @@ public class Reception extends JFrame implements ActionListener {
         btnNewButton_1.setBounds(10, 110, 200, 30);
         btnNewButton_1.setBackground(Color.BLACK);
         btnNewButton_1.setForeground(Color.WHITE);
+        btnNewButton_1.addActionListener(this);
         contentPane.add(btnNewButton_1);
 
         JButton btnNewButton_2 = new JButton("All Employee Info");
         btnNewButton_2.setBounds(10, 150, 200, 30);
         btnNewButton_2.setBackground(Color.BLACK);
         btnNewButton_2.setForeground(Color.WHITE);
+        btnNewButton_2.addActionListener(this);
         contentPane.add(btnNewButton_2);
 
         JButton btnNewButton_3 = new JButton("Customer Info");
         btnNewButton_3.setBounds(10, 190, 200, 30);
         btnNewButton_3.setBackground(Color.BLACK);
         btnNewButton_3.setForeground(Color.WHITE);
+        btnNewButton_3.addActionListener(this);
         contentPane.add(btnNewButton_3);
 
         JButton btnManagerInfo = new JButton("Manager Info");
         btnManagerInfo.setBounds(10, 230, 200, 30);
         btnManagerInfo.setBackground(Color.BLACK);
         btnManagerInfo.setForeground(Color.WHITE);
+        btnManagerInfo.addActionListener(this);
         contentPane.add(btnManagerInfo);
 
         JButton btnNewButton_4 = new JButton("Check Out");
         btnNewButton_4.setBounds(10, 270, 200, 30);
         btnNewButton_4.setBackground(Color.BLACK);
         btnNewButton_4.setForeground(Color.WHITE);
+        btnNewButton_4.addActionListener(this);
         contentPane.add(btnNewButton_4);
 
         JButton btnNewButton_5 = new JButton("Update Check Status");
         btnNewButton_5.setBounds(10, 310, 200, 30);
         btnNewButton_5.setBackground(Color.BLACK);
         btnNewButton_5.setForeground(Color.WHITE);
+        btnNewButton_5.addActionListener(this);
         contentPane.add(btnNewButton_5);
 
         JButton btnNewButton_6 = new JButton("Update Room Status");
         btnNewButton_6.setBounds(10, 350, 200, 30);
         btnNewButton_6.setBackground(Color.BLACK);
         btnNewButton_6.setForeground(Color.WHITE);
+        btnNewButton_6.addActionListener(this);
         contentPane.add(btnNewButton_6);
 
         JButton btnPickUpSerice = new JButton("Pick up Service");
         btnPickUpSerice.setBounds(10, 390, 200, 30);
         btnPickUpSerice.setBackground(Color.BLACK);
         btnPickUpSerice.setForeground(Color.WHITE);
+        btnPickUpSerice.addActionListener(this);
         contentPane.add(btnPickUpSerice);
 
         JButton btnSearchRoom = new JButton("Search Room");
         btnSearchRoom.setBounds(10, 430, 200, 30);
         btnSearchRoom.setBackground(Color.BLACK);
         btnSearchRoom.setForeground(Color.WHITE);
+        btnSearchRoom.addActionListener(this);
         contentPane.add(btnSearchRoom);
 
         JButton btnNewButton_7 = new JButton("Log Out");
         btnNewButton_7.setBounds(10, 470, 200, 30);
         btnNewButton_7.setBackground(Color.BLACK);
         btnNewButton_7.setForeground(Color.WHITE);
+        btnNewButton_7.addActionListener(this);
         contentPane.add(btnNewButton_7);
 
         getContentPane().setBackground(Color.WHITE);
@@ -112,7 +122,10 @@ public class Reception extends JFrame implements ActionListener {
                 case "Rooms":
                     new Room().setVisible(true);
                     break;
-                // to add more cases as necessary for other buttons
+                case "Department":
+                    new Department().setVisible(true);
+                    break;
+                // here add more cases as necessary for other buttons
                 default:
                     break;
             }
